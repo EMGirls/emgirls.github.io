50a0802fdf829671dfcb9024ab7cfa9579632cc2
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2021-09-08 18:49:08 -0400

    no backend yet

---
+++ js/contact-script.js
@@ -0,0 +1,9 @@
+var nob = document.getElementById("name-of-business");
+var email = document.getElementById("email");
+var message = document.getElementById("message");
+
+function onSend() {
+    let nobText = nob.value;
+    let emailText = email.value;
+    let messageText = message.value;
+}
