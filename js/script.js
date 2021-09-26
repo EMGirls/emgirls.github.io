c86d96bb789e9444c8ef6621512a6958252371fa
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2021-09-26 15:26:31 -0400

    i demand less blurry images!!!

--- js/script.js
+++ js/script.js
@@ -3,6 +3,12 @@ var girlsInvolved = document.getElementById("girls-involved");
 var countriesInvolved = document.getElementById("countries-involved");
 var mentorsInvolved = document.getElementById("mentors-involved");
 var impact = document.getElementById("impact");
+var slides = document.getElementById("slides");
+
+const images = ["/assets/images/index/unknown.png", 
+    "/assets/images/index/emintern_team_graphic.png", 
+    "/assets/images/index/now_hiring_website_graphic.png"];
+let index = 0;
 
 let count = [0, 0, 0];
 const finalCount = [3000, 50, 40];
@@ -24,4 +30,19 @@ function counter() {
     countriesInvolved.innerHTML = `${count[1]}+`;
     mentorsInvolved.innerHTML = `${count[2]}+`;
     setTimeout(counter, 1);
+}
+
+function changeImage() {
+    index %= images.length;
+    slides.src = images[index];
+}
+
+function nextImage() {
+    index++;
+    changeImage();
+}
+
+function idk() {
+    nextImage();
+    setTimeout(idk, 2000);
 }
